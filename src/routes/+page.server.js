6886c44ab44ceea7ac17d65a4e324389a0f54182
zainablfz <144009548+zainablfz@users.noreply.github.com>
2025-06-04 15:21:40 +0200

    feat: #19 fetched data from directus

---
+++ src/routes/+page.server.js
@@ -0,0 +1,32 @@
+export async function load() {
+  try {
+    const [verenigingenRes, activiteitenRes] = await Promise.all([
+      fetch('https://fdnd-agency.directus.app/items/leidensportzomer_verenigingen'),
+      fetch('https://fdnd-agency.directus.app/items/leidensportzomer_activiteiten')
+    ]);
+
+    if (!verenigingenRes.ok || !activiteitenRes.ok) throw new Error('Fout bij ophalen');
+
+    const verenigingenData = await verenigingenRes.json();
+    const activiteitenData = await activiteitenRes.json();
+
+    return {
+      verenigingen: verenigingenData.data,
+      activiteiten: activiteitenData.data
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      verenigingen: [],
+      activiteiten: []
+    };
+  }
+
+
+  
+}
+
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
+<script>
+  export let data;
+
+  const { verenigingen, activiteiten } = data;
+</script>
+
 <h1>Leiden sportzomer</h1>
+
+<h2>Verenigingen</h2>
+<ul>
+  {#each verenigingen as vereniging}
+    <li>{vereniging.naam}</li>
+  {/each}
+</ul>
+
+<h2>Activiteiten</h2>
+<ul>
+  {#each activiteiten as activiteit}
+    <article>
+      <h3>{activiteit.titel}</h3>
+      <p>{activiteit.locatie}</p>
+      <p>{activiteit.datum}</p>
+      <p>{activiteit.tijd}</p>
+    </article>
+  {/each}
+</ul>
